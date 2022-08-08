bdca2ed32d52bc84615f05d8ebe34701f5d8aee3
Jake <jakepucci26@gmail.com>
2022-08-08 17:43:31 -0600

    commit tree e20a0dc065a3fd784da641cf20faa8807167557c

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1= 31
+const num2= 2
+let multiply = num1 * num2 
+
+let random = parseInt(Math.random() * 10);
+
+const num3= 16
+const num4= 4
+let mod = num3/num4 
+
+let max= Math.max(20,10,2)
